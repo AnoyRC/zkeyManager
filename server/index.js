81c60af310c11984073f11ae431be319bcec3037
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-06-21 20:00:59 +0530

    Initializing Server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
